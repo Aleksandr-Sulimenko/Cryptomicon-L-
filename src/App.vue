1b61c70c845f804f5ed5a3eb422ba2d77e6e79a2
Aleksandr-Sulimenko <aleksandrsul1menko@yandex.ru>
2022-01-27 11:58:34 +0300

    selection and deselectionticker

--- src/App.vue
+++ src/App.vue
@@ -69,15 +69,20 @@
 
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
+        {{ sel }}
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
             v-for="t in tickers"
-            v-bind:key="t"
+            :key="t"
+            @click="sel = t"
+            :class="{
+              'border-4':sel === t
+            }"
+            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }}
+                {{ t.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
                 {{ t.price }}
@@ -85,7 +90,7 @@
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click="handleDelete(t)"
+              @click.stop="handleDelete(t)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -107,9 +112,9 @@
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section class="relative">
+      <section v-if="sel" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{ sel.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div class="bg-purple-800 border w-10 h-24"></div>
@@ -117,7 +122,11 @@
           <div class="bg-purple-800 border w-10 h-48"></div>
           <div class="bg-purple-800 border w-10 h-16"></div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        <button
+          @click="sel = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -154,18 +163,19 @@ export default {
       ticker: "default",
       tickers: [
         {
-          name: "DEMO",
+          name: "DEMO1",
           price: "-",
         },
         {
-          name: "DEMO",
+          name: "DEMO2",
           price: "-",
         },
         {
-          name: "DEMO",
+          name: "DEMO3",
           price: "-",
         },
       ],
+      sel: null,
     };
   },
 
