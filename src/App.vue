53bcaa995dc14ebbea13ec2f07c1d3ae21c26f70
Aleksandr-Sulimenko <aleksandrsul1menko@yandex.ru>
2022-02-03 13:41:48 +0300

    add saving to localStorage

--- src/App.vue
+++ src/App.vue
@@ -167,8 +167,33 @@ export default {
       graph: [],
     };
   },
-
+  created() {
+    const tickersData = localStorage.getItem("cryptomicon-list");
+    if (tickersData) {
+      console.log(tickersData);
+      this.tickers = JSON.parse(tickersData);
+      console.log(tickersData);
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=d4614669772e47a6b813f551a50d81607d94d3d82f9db18351fe1bd8b0c15d11`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 10000),
+        (this.ticker = "");
+    },
+
     add() {
       // if(evt.key === "Enter"){}
       const currentTicker = {
@@ -176,18 +201,8 @@ export default {
         price: "-",
       };
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=d4614669772e47a6b813f551a50d81607d94d3d82f9db18351fe1bd8b0c15d11`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 10000),
-        (this.ticker = "");
+      localStorage.setItem("cryptomicon-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
